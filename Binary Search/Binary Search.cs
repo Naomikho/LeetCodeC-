54f9c6d403db68c5cb006b4d3c9b825b42cd7327
Naomikho <khoyining9@gmail.com>
2022-07-19 11:23:18 +0800

    Create Binary Search.cs

---
+++ Binary Search/Binary Search.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.Length - 1;
+        
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            if (target == nums[mid])    return mid;
+            else if (target < nums[mid])     end = mid - 1;
+            else     start = mid + 1;
+        }
+        
+        return -1;
+    }
+    
+}
