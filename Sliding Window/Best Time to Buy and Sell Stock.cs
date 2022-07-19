325c3d223f233ee174e3e66e6c858b6aa2386480
Naomikho <khoyining9@gmail.com>
2022-07-19 11:19:56 +0800

    Update Best Time to Buy and Sell Stock.cs

--- Sliding Window/Best Time to Buy and Sell Stock.cs
+++ Sliding Window/Best Time to Buy and Sell Stock.cs
@@ -1,6 +1,9 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int min = prices[0];
+        //what the algorithm wants is only the biggest gap we can obtain from the buying and selling price
+        //hence whenever there is a new lowest buying price, we can store that as min instead
+        //because even if the calculation proceeds, if there is no new larger gap, the last recorded gap will not be updated.
+        int min = prices[0]; 
         int profit = 0;
         
         for (int i = 1; i < prices.Length; i++) {
