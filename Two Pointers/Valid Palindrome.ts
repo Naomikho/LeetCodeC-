61b716f98e95e3c17e5a151c14ca9d4494ce7ae7
Naomikho <khoyining9@gmail.com>
2023-07-26 22:09:36 +0800

    add comments on regex expression

--- Two Pointers/Valid Palindrome.ts
+++ Two Pointers/Valid Palindrome.ts
@@ -1,3 +1,13 @@
+/*
+/: This denotes the start and end of the regular expression pattern.
+
+[^a-zA-Z0-9]: The ^ symbol inside square brackets [] is a negation or "not" symbol. It means that we want to match any character that is NOT listed inside the square brackets.
+
+a-zA-Z0-9: These are character ranges inside the square brackets, representing all the uppercase and lowercase alphabets (a-z, A-Z) and all the digits (0-9). So, a-zA-Z0-9 matches any alphanumeric character.
+
+/g: The g flag stands for "global" and is used to perform a global search. It means that the regular expression will find all matches in the input string, rather than stopping after the first match.
+*/
+
 function isPalindrome(s: string): boolean {
     s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     const isEven = s.length % 2 == 0;
