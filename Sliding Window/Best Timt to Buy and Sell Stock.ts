f8b58b3eb8ec2db8adc6635c55806ef4004fd63a
Naomikho <khoyining9@gmail.com>
2023-07-27 23:44:06 +0800

    Update Best Timt to Buy and Sell Stock.ts

--- Sliding Window/Best Timt to Buy and Sell Stock.ts
+++ Sliding Window/Best Timt to Buy and Sell Stock.ts
@@ -1,3 +1,10 @@
+/*
+The thinking behind this solution is by knowing whether it is possibel to get an even better profit in the future. Hence, what we need to do is keep the current best profit.
+Firstly, we want to buy whichever that is the cheapest, but we also don't want to forgo what we bought if it's not possible to gain better profit. This is why the current highest profit is kept, and we only need to use
+one variable to keep the current lowest price.
+The next point is how to determine whether it is plausible to gain profit. Namely, you will gain profit when your selling price is higher than your latest buying price. But you also need to know whether this selling price
+nets you more profit than you previously can obtain(otherwise how would you get more profit?), which is why we check prices[i] - low > profit. If yes, we overwrite the latest selling price. 
+*/
 function maxProfit(prices: number[]): number {
   let low = prices[0];
   let high = prices[0];
