c9e73dc067f101fb4968d728f62d49e53cc287f1
Naomikho <khoyining9@proton.me>
2023-08-13 18:14:50 +0800

    Create Same Tree.ts

---
+++ Trees/Same Tree.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    function matchTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if (!p && !q) return true;
+        else if (p == null || q == null) return false;
+        else
+        {
+            if (p.val != q.val) return false;
+
+            const lTreeEqual = matchTree(p.left, q.left);
+            const rTreeEqual = matchTree(p.right, q.right);
+
+            return lTreeEqual && rTreeEqual;
+        }
+    }
+
+    return matchTree(p, q);
+};
